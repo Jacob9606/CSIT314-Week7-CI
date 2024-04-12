b08e8dbf90491294485ce2457373d0687d743c11
Myunggyun Yu (Jacob) <64580113+Jacob9606@users.noreply.github.com>
2024-04-12 13:12:36 +1000

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(1, 2) == 3
     assert add(5, -3) == 2
     assert add(-1, -1) == -2
-    assert add(0, 0) == 0
+    assert add(0, 0) == 0 
 
 def test_subtract():
     assert subtract(10, 5) == 5
